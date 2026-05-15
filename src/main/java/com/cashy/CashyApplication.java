c233dbdb92abd59a1a71c79e51e94d28d351e7ed
ViktoriaMancheva <viktoriyamanchevaa@gmail.com>
2026-05-15 15:30:38 +0300

    add packages

---
+++ src/main/java/com/cashy/CashyApplication.java
@@ -0,0 +1,12 @@
+package com.cashy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CashyApplication.class, args);
+    }
+
+}
